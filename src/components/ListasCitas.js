78e3f73a26beded9984fae3ca85133b2d30d16d5
Luis Perez Mosteiro <lperez@NTB-189.local>
2020-01-13 21:24:26 -0300

    Añadiendo estado del componente lista de citas

--- src/components/ListasCitas.js
+++ src/components/ListasCitas.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Cita from './Cita';
 
-const ListasCitas = ({citas, eliminarCita}) => (
+const ListasCitas = ({citas, eliminarCita}) => {
+
+    // imprimir un mensaje en base si hay citas o no
+    const mensaje = Object.keys(citas).length === 0 ? 'No hay citas' : 'Administra las citas aqui';
+
+    return (
         <div className="card mt-2 py-5">
             <div className="card-body">
-                <h2 className="card-title text-center">Administra las citas aqui</h2>
+            <h2 className="card-title text-center">{mensaje}</h2>
 
                 <div className="lista-citas">
                     {citas.map(cita => (
@@ -19,6 +24,7 @@ const ListasCitas = ({citas, eliminarCita}) => (
 
             </div>
         </div>
-);
+    );
+}
 
 export default ListasCitas;
